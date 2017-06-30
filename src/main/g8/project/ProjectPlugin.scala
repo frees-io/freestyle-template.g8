22aa350dbe6a4600155d28a6e493908d3885d466
AdrianRaFo <adrianramirezfornell@gmail.com>
2017-06-30 14:13:11 +0200

    Move scalaMeta settings

--- src/main/g8/project/ProjectPlugin.scala
+++ src/main/g8/project/ProjectPlugin.scala
@@ -5,6 +5,4 @@ object ProjectPlugin extends AutoPlugin {
   override def requires: Plugins = FreestylePlugin
 
   override def trigger: PluginTrigger = allRequirements
-
-  override def projectSettings: Seq[Def.Setting[_]] = scalaMetaSettings
 }
